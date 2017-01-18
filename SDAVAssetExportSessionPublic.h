629e912bff44ac555077eed961f55eab54f64441
Jon Andersen <jon@andersen.re>
2017-01-17 22:33:01 -0500

    Carthage support

---
+++ SDAVAssetExportSessionPublic.h
@@ -0,0 +1,19 @@
+//
+//  SDAVAssetExportSession.h
+//  SDAVAssetExportSession
+//
+//  Created by Jon Andersen on 1/17/17.
+//  Copyright © 2017 Jon Andersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDAVAssetExportSession.
+FOUNDATION_EXPORT double SDAVAssetExportSessionVersionNumber;
+
+//! Project version string for SDAVAssetExportSession.
+FOUNDATION_EXPORT const unsigned char SDAVAssetExportSessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDAVAssetExportSession/PublicHeader.h>
+
+#import <SDAVAssetExportSession/SDAVAssetExportSession.h>
